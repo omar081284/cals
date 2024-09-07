36fbf9e3ee60013f50bdf1688402c4a64afd2a2f
omar081284 <omarmahrous20105@gmail.com>
2024-09-07 14:50:13 +0300

    add the project

---
+++ season 2.js
@@ -0,0 +1,14 @@
+
+function getnum(num){
+    document.getElementById('result').innerHTML=num;
+}
+function sum(){
+return n1+n2;
+}
+function min(){
+
+return n1-n2
+}
+function equal(){
+    sum(n1,n2)
+}
